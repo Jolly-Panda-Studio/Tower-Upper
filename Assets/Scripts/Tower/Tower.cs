9395730c39889304e918d42c2486dd5a954214d2
Joseph Farahmand <50228817+JosephFarahmand@users.noreply.github.com>
2022-12-16 13:48:19 +0330

    Create Tower.cs

---
+++ Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,22 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(TowerComponents))]
+public class Tower : MonoBehaviour
+{
+    public static Tower Instance { get; private set; }
+
+    [SerializeField] private TowerComponents m_Components;
+
+    public TowerComponents Components => m_Components;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        m_Components ??= gameObject.GetOrAddComponent<TowerComponents>();
+    }
+}
