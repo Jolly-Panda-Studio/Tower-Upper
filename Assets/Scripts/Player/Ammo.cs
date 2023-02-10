997246b1d2d693f87f86c17c18ff73b2651b537a
Usef Farahmand <50228817+JosephFarahmand@users.noreply.github.com>
2023-02-10 17:25:19 +0330

    fix add trigger function in bullet

--- Assets/Scripts/Player/Ammo.cs
+++ Assets/Scripts/Player/Ammo.cs
@@ -1,6 +1,24 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class Ammo : MonoBehaviour
 {
+    [SerializeField] private float force;
 
+    private void Start()
+    {
+        var collider = GetComponent<Collider>();
+        collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Enemy enemy))
+        {
+            enemy.Kill();
+
+            var rigid = enemy.gameObject.GetOrAddComponent<Rigidbody>();
+            rigid.AddForce(Vector3.down * force, ForceMode.Impulse);
+        }
+    }
 }
