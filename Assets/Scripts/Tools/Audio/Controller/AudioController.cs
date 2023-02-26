62129376644f343e06da717c817a055c47f4d66c
Usef Farahmand <50228817+JosephFarahmand@users.noreply.github.com>
2023-02-26 17:32:04 +0330

    feat create audio controller and audio database

---
+++ Assets/Scripts/Tools/Audio/Controller/AudioController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Lindon.Framwork.Audio.Controller
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AudioController : MonoBehaviour
+    {
+        public static AudioController Instance { get; private set; }
+
+        private AudioDatabase m_Database;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+
+                LoadComponents();
+                LoadDatabase();
+
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void LoadComponents()
+        {
+            m_Database = new AudioDatabase();
+        }
+
+        private void LoadDatabase()
+        {
+            m_Database.Load();
+        }
+
+        private void SaveDatabase()
+        {
+            m_Database.Save();
+        }
+
+        private void OnApplicationQuit()
+        {
+            SaveDatabase();
+        }
+    }
+}
---
+++ Assets/Scripts/Tools/Audio/Controller/AudioDatabase.cs
@@ -0,0 +1,52 @@
+﻿using Lindon.Framwork.Audio.Data;
+using Lindon.Framwork.Audio.Event;
+using Lindon.Framwork.Database;
+using System;
+
+namespace Lindon.Framwork.Audio.Controller
+{
+    public class AudioDatabase
+    {
+        public static event Action<AudioController> OnLoad;
+
+        private string GetTag(AudioSourceType value, string extension)
+        {
+            var tag = $"{value.ToString().ToUpper()}_{extension}";
+            return tag;
+        }
+
+        public void Load()
+        {
+            foreach (var value in Enum.GetValues(typeof(AudioSourceType)))
+            {
+                var key = (AudioSourceType)value;
+
+                string muteTag = GetTag((AudioSourceType)value, "MUTE");
+                var isMute = DatabaseHandler.GetBool(muteTag);
+                AudioMuter.SetMute(key, isMute);
+
+                string volumeTag = GetTag((AudioSourceType)value, "VOLUME");
+                var volume = DatabaseHandler.GetFloat(volumeTag);
+                AudioVolumeChanger.ChangeVolume(key, volume);
+            }
+
+            OnLoad?.Invoke(AudioController.Instance);
+        }
+
+        public void Save()
+        {
+            foreach (var value in Enum.GetValues(typeof(AudioSourceType)))
+            {
+                var key = (AudioSourceType)value;
+
+                string muteTag = GetTag((AudioSourceType)value, "MUTE");
+                var isMute = AudioMuter.IsMute(key);
+                DatabaseHandler.SetBool(muteTag, isMute);
+
+                string volumeTag = GetTag((AudioSourceType)value, "VOLUME");
+                var volume = AudioVolumeChanger.GetVolume(key);
+                DatabaseHandler.SetFloat(volumeTag, volume);
+            }
+        }
+    }
+}
