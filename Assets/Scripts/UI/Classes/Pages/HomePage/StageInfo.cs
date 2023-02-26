f7e229a9998b9adc2d37cf34bab1dd877668a328
Usef Farahmand <50228817+JosephFarahmand@users.noreply.github.com>
2023-02-26 10:14:28 +0330

    fix optimize script

--- Assets/Scripts/UI/Classes/Pages/HomePage/StageInfo.cs
+++ Assets/Scripts/UI/Classes/Pages/HomePage/StageInfo.cs
@@ -2,8 +2,10 @@ using DG.Tweening;
 using Lindon.TowerUpper.GameController.Events;
 using Lindon.TowerUpper.GameController.Level;
 using Lindon.TowerUpper.Profile;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static Cinemachine.DocumentationSortingAttribute;
 
 namespace Lindon.UserManager.Page.Home
 {
@@ -13,9 +15,15 @@ namespace Lindon.UserManager.Page.Home
         [SerializeField] private Transform m_Parent;
         [SerializeField] private LevelSlot m_Prefab;
         [SerializeField] private LevelSlot m_BossPrefab;
+        private List<LevelSlot> m_Slots;
 
         [Space]
         [SerializeField] private Slider m_slider;
+
+        private void Start()
+        {
+            InstantiateSlot();
+        }
 
         private void OnEnable()
         {
@@ -37,23 +45,16 @@ namespace Lindon.UserManager.Page.Home
         private void Load(Profile profile)
         {
             var info = profile.GetChapter();
-            m_Parent.DestroyChildren();
 
             SliderAnimation(info.GameLevel);
+
+            InstantiateSlot();
 
             for (int i = 0; i < 10; i++)
             {
                 var bossLevel = i == 9;
 
-                LevelSlot slot;
-                if (!bossLevel)
-                {
-                    slot = Instantiate(m_Prefab, m_Parent);
-                }
-                else
-                {
-                    slot = Instantiate(m_BossPrefab, m_Parent);
-                }
+                LevelSlot slot = m_Slots[i];
 
                 var level = i + (10 * info.ChapterLevel);
 
@@ -71,9 +72,31 @@ namespace Lindon.UserManager.Page.Home
                     data = new LevelSlot.Data(level, LevelSlot.Data.State.Future, bossLevel);
                 }
 
-
-
                 slot.SetData(data);
+            }
+        }
+
+        private void InstantiateSlot()
+        {
+            if (m_Slots != null) return;
+
+            m_Slots = new List<LevelSlot>();
+            for (int i = 0; i < 10; i++)
+            {
+                var bossLevel = i == 9;
+
+                LevelSlot slot;
+                if (!bossLevel)
+                {
+                    slot = Instantiate(m_Prefab, m_Parent);
+                }
+                else
+                {
+                    slot = Instantiate(m_BossPrefab, m_Parent);
+                }
+                var data = new LevelSlot.Data(-1, LevelSlot.Data.State.Future, bossLevel);
+                slot.SetData(data);
+                m_Slots.Add(slot);
             }
         }
 
